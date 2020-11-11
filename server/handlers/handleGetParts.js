ccbcdedac76d7869411dd7892f7ce7bf6da67cbd
Alex Langevin <alex_langevin@hotmail.ca>
2020-11-10 21:11:15 -0500

    athlete id and gear_id array

--- server/handlers/handleGetParts.js
+++ server/handlers/handleGetParts.js
@@ -1,10 +1,22 @@
+//This function: 
+//- connects to the Database named "MyApp" and then the collection called "parts"
+//- it returns an array of objects corresponding to bicycle parts.
+//- here is an example of part:
+// {
+// _id:"tr-gatorskin-25"
+// diameter:"700c"
+// width:"25mm"
+// brand:"continental"
+// msrp:"69.99"
+// lifespan:"4000"
+// }
+
 'use strict';
 //MongoDB stuff
 require('dotenv').config({path: '../.env'});
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
 const { MONGO_URI } = process.env;
-console.log(MONGO_URI);
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/handlers/handleUpdateUserInfo.js
+++ server/handlers/handleUpdateUserInfo.js
@@ -3,10 +3,25 @@ require('dotenv').config({ path: '../.env' });
 const handleGetActivities = require('./handleGetActivities');
 
 const handleUpdateUserInfo = async (req,res) => {
-  //Info on my Strava account?
 const userInfo = await handleGetActivities();
-console.log(userInfo);
+
+//This will give us the id of the user named athleteId
+const athleteId = await userInfo[0].athlete.id;
+console.log("athleteId", athleteId);
+
+//This will give us an array of all the bicycle gear_ids of the athlete
+let bicycleIdArray = [];
+await userInfo.forEach( data => {
+  const bicycleId = data.gear_id;
+  if(!bicycleIdArray.includes(bicycleId)){
+    bicycleIdArray.push(bicycleId);
+  }
+});
+console.log("bicycles: ", bicycleIdArray);
+
+
 res.status(200).json({status:200, user_info: userInfo});
+
 }
 
 module.exports = handleUpdateUserInfo;
